4d6790cd0d7b38b98befa97d2784807fb16865a5
Ding's GitHub <yuqi2.25@intl.zju.edu.cn>
2026-03-11 15:18:50 +0800

    edit infection

--- Practical 4/infection.py
+++ Practical 4/infection.py
@@ -1,10 +1,12 @@
 # give the initial students number, the rate, the total students number, and the initial days
-inflected=5.   
+inflected=5  
 rate=0.4
 total=91
 days=1
+print(f"day: {days}, inflected students:{inflected}")
 # use while-loop to compare inflected and the total
 while inflected < total:   
     inflected=inflected*(1+rate)     #calculate the inflected number now
     days+=1   #remember to +1 of the days
+    print(f"day: {days}, inflected students:{inflected}")
 print(days)
