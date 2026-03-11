b73269a79709012d35657b2c5dc5a067f8eee6f6
Ding's GitHub <yuqi2.25@intl.zju.edu.cn>
2026-03-11 14:54:51 +0800

    edit Practical 4

--- Practical 4/variables.py
+++ Practical 4/variables.py
@@ -1,10 +1,17 @@
 a=5.08
 b=5.33
 c=5.55
-d=0.25
-e=0.22
+d=b-a
+e=c-b
+if d>e:
+    print("population growth is decelerating in Scotland")
+elif d<e:
+    print("population growth is accelerating in Scotland")
+else:
+    print("population growth is not change in Scotland")
 #d>e, population growth is decelerating in Scotland
 X=True
 Y=False
 W=X or Y
+print(W)
 # the truth table for W: W = True
